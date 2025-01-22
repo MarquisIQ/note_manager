70ca32115ce68c2ef8bd841bc2bc682b61bff451
MarquisIQ <forworkzhukov@gmail.com>
2025-01-22 15:12:25 +0300

    Grade 1. Этап 1: Задание 1

---
+++ greetings.py
@@ -0,0 +1,14 @@
+username = 'MarquisIQ' #имя пользователя
+title = 'note_name' #заголовок заметки
+content = 'content_description' #описание заметки
+status = 'note_status' #статус заметки
+created_date = '22-01-2025' #дата создания заметки
+issue_date = '22-02-2025' #дата истечения заметки
+
+
+print("Имя пользователя:", username)
+print('Заголовок заметки', title)
+print('Описание заметки', content)
+print('Статус заметки', status)
+print('Дата создания заметки', created_date)
+print('Дата истечения заметки', issue_date)
