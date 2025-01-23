1878de4eb7d25c0da9374985566c3c28f8d4abbf
MarquisIQ <forworkzhukov@gmail.com>
2025-01-23 17:14:18 +0300

    Добавил переменные и вывод под шаблон в date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,5 +1,13 @@
-created_date = '23-01-2025'
-issue_date = '23-02-2025'
+username = 'MarquisIQ' #имя пользователя
+title = 'note_name' #заголовок заметки
+content = 'content_description' #описание заметки
+status = 'note_status' #статус заметки
+created_date = '23-01-2025' #дата создания заметки
+issue_date = '23-02-2025' #дата истечения заметки
 
+print("Имя пользователя:", username)
+print('Заголовки заметки', title)
+print('Описание заметки', content)
+print('Статус заметки', status)
 print('Дата создания заметки', created_date[:-5])
 print('Дата истечения заметки', issue_date[:-5])
